2711bc9479e695a232b7bffea1dbcfac4c8cadea
Ruben Espino <56930001+r-o-e-574@users.noreply.github.com>
2020-06-26 12:50:33 -0500

    made code pass flake8

--- logpuzzle.py
+++ logpuzzle.py
@@ -19,7 +19,7 @@ __author__ = "Ruben Espino with help from Chris Warren"
 import os
 import re
 import sys
-#checks to see if appropriate python version is installed
+# checks to see if appropriate python version is installed
 if sys.version_info[0] >= 3:
     from urllib.request import urlretrieve
 else:
@@ -42,19 +42,19 @@ def read_urls(filename):
     url_list = create_urls(puzzle_urls)
     url_list = list(set(url_list))
     sorted_urls = sorted(url_list, key=return_last_word)
-    
-    
+
     def extract_host_name(url):
         """returns the host name from a given url"""
     host = re.findall(r'GET (\S+) HTTP', url)
-    
+    print(host)
+
     return sorted_urls
 
 
 def create_urls(urls):
     front = 'http://code.google.com'
     url_return = [front + url for url in urls]
-    
+
     return url_return
 
 
@@ -66,7 +66,7 @@ def add_prefixes(filename, host_list):
     """adds server prefixes to the urls in host_list"""
     server_name = "https://" + re.findall(r'\S+\_(\S+)', filename)[0]
     completed_url_list = [server_name + host for host in host_list]
-    
+
     return completed_url_list
 
 
@@ -88,11 +88,11 @@ def download_images(img_urls, dest_dir):
         urlretrieve(url, dest_dir + "/" + image_name)
         index_html += '<img src={}></img>'.format(image_name)
     index_html += '</body></html>'
-    
+
     with open(dest_dir + '/index.html', 'w') as w_index:
         w_index.write(index_html)
-        
-        
+
+
 def create_parser():
     """Creates an argument parser object."""
     parser = argparse.ArgumentParser()
@@ -114,7 +114,7 @@ def main(args):
         download_images(img_urls, parsed_args.todir)
     else:
         print('\n'.join(img_urls))
-        
-        
+
+
 if __name__ == '__main__':
     main(sys.argv[1:])
